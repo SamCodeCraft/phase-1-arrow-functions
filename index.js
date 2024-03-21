a1986c6583aed4cf7b3eec64cc1298956dfefa12
siyajari samuel <siyajari.samuel@student.moringaschool.com>
2024-03-21 17:56:52 +0300

    Created Arrow Function

---
+++ index.js
@@ -0,0 +1,17 @@
+// Define the divide function expression
+const divide = function(a, b) {
+    return  a / b;
+  };
+  
+  // Define the square arrow function
+  const square = (x) => {
+    return x * x;
+  };
+  
+  // Define the add arrow function
+  const add = (a, b) => {
+    return a + b;
+  };
+  
+  
+  
